f46249ceda7dc42d15e73b10bc5d6e4e3e7a9ff6
Fabio Dorneles <42509240+fabiodrneles@users.noreply.github.com>
2021-11-22 19:18:28 -0300

    Update index.js
    editando codigo

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ function gameEngine(){
         gameOverSound.play();
         musicSound.pause();
         inputDir =  {x: 0, y: 0}; 
-        alert("Game Over. Press any key to play again!");
+        alert("Game Over!! Pressione qualquer tecla para jogar novamente!");
         snakeArr = [{x: 13, y: 15}];
         musicSound.play();
         score = 0; 
